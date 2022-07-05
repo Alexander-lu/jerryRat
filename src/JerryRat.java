58f6472d262801531e2d4c8c1c6dcc1d67876e9c
Alexander68Lu <81604280@qq.com>
2022-07-05 14:52:02 +0800

    修改了JerryRat,实现了传输文件

--- src/JerryRat.java
+++ src/JerryRat.java
@@ -1,11 +1,43 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class JerryRat implements Runnable {
-    @Override
-    public void run() {
-        System.out.println("I'm Jerry the rat!");
+    public static final String SERVER_PORT = "8080";
+    ServerSocket serverSocket;
+    public JerryRat() throws IOException {
+        serverSocket = new ServerSocket(Integer.parseInt(SERVER_PORT));
     }
 
-    public static void main(String[] args){
+
+
+    @Override
+    public void run() {
+        try (
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+            String s = in.readLine();
+            while (s!=null) {
+                String[] s1 = s.split(" ");
+                String s2 = s1[1];
+                Scanner scanner = new Scanner(new File("res/webroot" + s2));
+                String a = scanner.nextLine();
+                while (scanner.hasNext()) {
+                    a += scanner.nextLine();
+                }
+                out.println(a);
+                s = in.readLine();
+            }
+    } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
         JerryRat jerryRat = new JerryRat();
-        jerryRat.run();
+        new Thread(jerryRat).run();
     }
 }
