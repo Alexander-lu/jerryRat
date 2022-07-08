0fc6db016c73b3c22552e704e2d5e191777c7a2f
Alexander68Lu <81604280@qq.com>
2022-07-08 19:04:12 +0800

    增加了0.9支持

--- src/JerryRat.java
+++ src/JerryRat.java
@@ -27,6 +27,158 @@ public class JerryRat implements Runnable {
                     if (OOOO==13) {
                         OOOO=inputStream.read();
                         if (OOOO==10) {
+                            a.add((byte)13);
+                            a.add((byte)10);
+                            break;
+                        }
+                    }else{
+                        a.add((byte)OOOO);
+                    }
+                }
+                byte[] bytes = new byte[a.size()];
+                for (int i = 0; i < a.size(); i++) {
+                    bytes[i] = a.get(i);
+                }
+                String bytes2String = new String(bytes, "UTF-8");
+                String[] split = bytes2String.split("\r\n",2);
+                String st = split[0];
+                if (st.startsWith("GET") | st.startsWith("get") | st.startsWith("HEAD") | st.startsWith("head")) {
+                    boolean ifHead = false;
+                    if (st.startsWith("HEAD") | st.startsWith("head")) {
+                        ifHead = true;
+                    }
+                    boolean ifOld = true;
+                    String s2;
+                    String s = URLDecoder.decode(st, "utf-8");
+                    String s1;
+                    if(ifHead){
+                        s1 = s.substring(5);
+                    }else {
+                        s1 = s.substring(4);
+                    }
+
+                    if (st.endsWith("HTTP/1.0") | st.endsWith("HTTP/1.1")) {
+                        ifOld = false;
+                        s2 = s1.substring(0, s1.length() - 9);
+                    } else {
+                        s2 = s1;
+                    }
+                    if (s2.equals("/endpoints/user-agent")) {
+                        split = split[1].split("\r\n",2);
+                        String userAgent = split[0];
+                        while (!userAgent.startsWith("User-Agent")) {
+                            split = split[1].split("\r\n",2);
+                            userAgent = split[0];
+                        }
+                        String substring = userAgent.substring(12);
+                        SimpleDateFormat sdf = new SimpleDateFormat("EEE, dd MMM yyyy hh:mm:ss z", Locale.ENGLISH);
+                        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+                        String str = sdf.format(new Date());
+                        clientSocket.getOutputStream().write(("HTTP/1.0 200 OK" + "\r\n" +
+                                "Date: " + str + "\r\n" + "Server: Apache/11.0" + "\r\n" +
+                                "Content-Length: " + substring.length() + "\r\n" + "Content-Type: " +
+                                "" + "text/html" + ";charset=utf-8" + "\r\n" + "\r\n").getBytes());
+                        clientSocket.getOutputStream().write(substring.getBytes());
+                    } else {
+                        String pathname = "res/webroot" + s2;
+                        File file = new File(pathname);
+                        String fileName = "";
+                        if (file.isDirectory()) {
+                            pathname = "res/webroot" + s2 + "/index.html";
+                            File file1 = new File(pathname);
+                            fileName = file1.getName();
+                        } else {
+                            fileName = file.getName();
+                        }
+                        String contentType;
+                        String[] splitName = fileName.split("\\.");
+                        if (splitName.length == 1) {
+                            contentType = "text/html";
+                        } else {
+                            String s3 = splitName[splitName.length - 1];
+                            switch (s3) {
+                                case "pdf":
+                                    contentType = "application/pdf";
+                                    break;
+                                case "ai":
+                                    contentType = "application/postscript";
+                                    break;
+                                case "xml":
+                                    contentType = "application/atom+xml";
+                                    break;
+                                case "json":
+                                    contentType = "application/json";
+                                    break;
+                                case "doc":
+                                    contentType = "application/msword";
+                                    break;
+                                case "js":
+                                    contentType = "application/javascript";
+                                    break;
+                                case "css":
+                                    contentType = "text/css";
+                                    break;
+                                case "html":
+                                    contentType = "text/html";
+                                    break;
+                                case "jpg":
+                                    contentType = "image/jpeg";
+                                    break;
+                                case "tiff":
+                                    contentType = "image/tiff";
+                                    break;
+                                case "gif":
+                                    contentType = "image/gif";
+                                    break;
+                                case "png":
+                                    contentType = "image/png";
+                                    break;
+                                case "jpeg":
+                                    contentType = "image/jpeg";
+                                    break;
+                                case "wbmp":
+                                    contentType = "image/vnd.wap.wbmp";
+                                    break;
+                                case "jpe":
+                                    contentType = "image/jpeg";
+                                    break;
+                                default:
+                                    contentType = "text/html";
+                                    break;
+                            }
+                        }
+                        try {
+                            FileInputStream fr = new FileInputStream(pathname);
+                            SimpleDateFormat sdf = new SimpleDateFormat("EEE, dd MMM yyyy hh:mm:ss z", Locale.ENGLISH);
+                            sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+                            String str = sdf.format(new Date());
+                            File fileLastTime = new File(pathname);
+                            long l = fileLastTime.lastModified();
+                            long length = fileLastTime.length();
+                            if (!ifOld) {
+                                clientSocket.getOutputStream().write(("HTTP/1.0 200 OK" + "\r\n" + "Date: " + str + "\r\n" + "Server: Apache/11.0" + "\r\n" + "Content-Length: " + length + "\r\n" + "Content-Type: " + contentType + ";charset=utf-8" + "\r\n" + "Last-Modified: " + sdf.format(new Date(l)) + "\r\n" + "\r\n").getBytes());
+                            }
+                            byte[] chs = new byte[1024];
+                            int len;
+                            while ((len = fr.read(chs)) != -1) {
+                                if (!ifHead) {
+                                    clientSocket.getOutputStream().write(chs, 0, len);
+                                }
+                            }
+                            fr.close();
+                        } catch (FileNotFoundException e) {
+                            clientSocket.getOutputStream().write(("HTTP/1.0 404 Not Found" + "\r\n" + "\r\n").getBytes());
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                else if (st.startsWith("POST")|st.startsWith("post")){
+                while ((OOOO=inputStream.read()) != -1){
+                    if (OOOO==13) {
+                        OOOO=inputStream.read();
+                        if (OOOO==10) {
                             OOOO=inputStream.read();
                             a.add((byte)13);
                             a.add((byte)10);
@@ -41,150 +193,17 @@ public class JerryRat implements Runnable {
                         a.add((byte)OOOO);
                     }
                 }
-                byte[] bytes = new byte[a.size()];
+                bytes = new byte[a.size()];
                 for (int i = 0; i < a.size(); i++) {
                     bytes[i] = a.get(i);
                 }
-                String bytes2String = new String(bytes, "UTF-8");
-                String[] split = bytes2String.split("\r\n",2);
-                String st = split[0];
-                    if (st.startsWith("GET") | st.startsWith("get") | st.startsWith("HEAD") | st.startsWith("head")) {
-                        boolean ifHead = false;
-                        if (st.startsWith("HEAD") | st.startsWith("head")) {
-                            ifHead = true;
-                        }
-                        boolean ifOld = true;
-                        String s2;
-                        String s = URLDecoder.decode(st, "utf-8");
-                        String s1;
-                        if(ifHead){
-                            s1 = s.substring(5);
-                        }else {
-                            s1 = s.substring(4);
-                        }
-
-                        if (st.endsWith("HTTP/1.0") | st.endsWith("HTTP/1.1")) {
-                            ifOld = false;
-                            s2 = s1.substring(0, s1.length() - 9);
-                        } else {
-                            s2 = s1;
-                        }
-                        if (s2.equals("/endpoints/user-agent")) {
-                            split = split[1].split("\r\n",2);
-                            String userAgent = split[0];
-                            while (!userAgent.startsWith("User-Agent")) {
-                                split = split[1].split("\r\n",2);
-                                userAgent = split[0];
-                            }
-                            String substring = userAgent.substring(12);
-                            SimpleDateFormat sdf = new SimpleDateFormat("EEE, dd MMM yyyy hh:mm:ss z", Locale.ENGLISH);
-                            sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-                            String str = sdf.format(new Date());
-                            clientSocket.getOutputStream().write(("HTTP/1.0 200 OK" + "\r\n" +
-                                    "Date: " + str + "\r\n" + "Server: Apache/11.0" + "\r\n" +
-                                    "Content-Length: " + substring.length() + "\r\n" + "Content-Type: " +
-                                    "" + "text/html" + ";charset=utf-8" + "\r\n" + "\r\n").getBytes());
-                            clientSocket.getOutputStream().write(substring.getBytes());
-                        } else {
-                            String pathname = "res/webroot" + s2;
-                            File file = new File(pathname);
-                            String fileName = "";
-                            if (file.isDirectory()) {
-                                pathname = "res/webroot" + s2 + "/index.html";
-                                File file1 = new File(pathname);
-                                fileName = file1.getName();
-                            } else {
-                                fileName = file.getName();
-                            }
-                            String contentType;
-                            String[] splitName = fileName.split("\\.");
-                            if (splitName.length == 1) {
-                                contentType = "text/html";
-                            } else {
-                                String s3 = splitName[splitName.length - 1];
-                                switch (s3) {
-                                    case "pdf":
-                                        contentType = "application/pdf";
-                                        break;
-                                    case "ai":
-                                        contentType = "application/postscript";
-                                        break;
-                                    case "xml":
-                                        contentType = "application/atom+xml";
-                                        break;
-                                    case "json":
-                                        contentType = "application/json";
-                                        break;
-                                    case "doc":
-                                        contentType = "application/msword";
-                                        break;
-                                    case "js":
-                                        contentType = "application/javascript";
-                                        break;
-                                    case "css":
-                                        contentType = "text/css";
-                                        break;
-                                    case "html":
-                                        contentType = "text/html";
-                                        break;
-                                    case "jpg":
-                                        contentType = "image/jpeg";
-                                        break;
-                                    case "tiff":
-                                        contentType = "image/tiff";
-                                        break;
-                                    case "gif":
-                                        contentType = "image/gif";
-                                        break;
-                                    case "png":
-                                        contentType = "image/png";
-                                        break;
-                                    case "jpeg":
-                                        contentType = "image/jpeg";
-                                        break;
-                                    case "wbmp":
-                                        contentType = "image/vnd.wap.wbmp";
-                                        break;
-                                    case "jpe":
-                                        contentType = "image/jpeg";
-                                        break;
-                                    default:
-                                        contentType = "text/html";
-                                        break;
-                                }
-                            }
-                            try {
-                                FileInputStream fr = new FileInputStream(pathname);
-                                SimpleDateFormat sdf = new SimpleDateFormat("EEE, dd MMM yyyy hh:mm:ss z", Locale.ENGLISH);
-                                sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-                                String str = sdf.format(new Date());
-                                File fileLastTime = new File(pathname);
-                                long l = fileLastTime.lastModified();
-                                long length = fileLastTime.length();
-                                if (!ifOld) {
-                                    clientSocket.getOutputStream().write(("HTTP/1.0 200 OK" + "\r\n" + "Date: " + str + "\r\n" + "Server: Apache/11.0" + "\r\n" + "Content-Length: " + length + "\r\n" + "Content-Type: " + contentType + ";charset=utf-8" + "\r\n" + "Last-Modified: " + sdf.format(new Date(l)) + "\r\n" + "\r\n").getBytes());
-                                }
-                                byte[] chs = new byte[1024];
-                                int len;
-                                while ((len = fr.read(chs)) != -1) {
-                                    if (!ifHead) {
-                                        clientSocket.getOutputStream().write(chs, 0, len);
-                                    }
-                                }
-                                fr.close();
-                            } catch (FileNotFoundException e) {
-                                clientSocket.getOutputStream().write(("HTTP/1.0 404 Not Found" + "\r\n" + "\r\n").getBytes());
-                                e.printStackTrace();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                    else if (st.startsWith("POST")|st.startsWith("post")){
-                        String afterDecode = URLDecoder.decode(st, "utf-8");
-                        String deletePost = afterDecode.substring(5);
-                        String url = deletePost.substring(0, deletePost.length() - 9);
-                        if (url.equals("/endpoints/null")) {
+                bytes2String = new String(bytes, "UTF-8");
+                split = bytes2String.split("\r\n",2);
+                st = split[0];
+                String afterDecode = URLDecoder.decode(st, "utf-8");
+                String deletePost = afterDecode.substring(5);
+                String url = deletePost.substring(0, deletePost.length() - 9);
+                if (url.equals("/endpoints/null")) {
                             File file = new File("res/webroot/null");
                             if(!file.exists()){
                                 file.createNewFile();
