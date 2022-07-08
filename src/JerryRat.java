5028f1c470d9f0b5920d097bd2916c43b295d959
Alexander68Lu <81604280@qq.com>
2022-07-09 03:53:09 +0800

    增加了400

--- src/JerryRat.java
+++ src/JerryRat.java
@@ -14,24 +14,25 @@ public class JerryRat implements Runnable {
     public JerryRat() throws IOException {
         serverSocket = new ServerSocket(Integer.parseInt(SERVER_PORT));
     }
-
     @Override
     public void run() {
         while (true) {
-            try (Socket clientSocket = serverSocket.accept();) {
+            try (
+                    Socket clientSocket = serverSocket.accept();
+            ) {
                 ArrayList<Byte> a = new ArrayList<>();
                 InputStream inputStream = clientSocket.getInputStream();
                 int OOOO;
-                while ((OOOO = inputStream.read()) != -1) {
-                    if (OOOO == 13) {
-                        OOOO = inputStream.read();
-                        if (OOOO == 10) {
-                            a.add((byte) 13);
-                            a.add((byte) 10);
+                while ((OOOO=inputStream.read()) != -1){
+                    if (OOOO==13) {
+                        OOOO=inputStream.read();
+                        if (OOOO==10) {
+                            a.add((byte)13);
+                            a.add((byte)10);
                             break;
                         }
-                    } else {
-                        a.add((byte) OOOO);
+                    }else{
+                        a.add((byte)OOOO);
                     }
                 }
                 byte[] bytes = new byte[a.size()];
@@ -39,7 +40,7 @@ public class JerryRat implements Runnable {
                     bytes[i] = a.get(i);
                 }
                 String bytes2String = new String(bytes, "UTF-8");
-                String[] split = bytes2String.split("\r\n", 2);
+                String[] split = bytes2String.split("\r\n",2);
                 String st = split[0];
                 if (st.startsWith("GET") | st.startsWith("get") | st.startsWith("HEAD") | st.startsWith("head")) {
                     boolean ifHead = false;
@@ -50,9 +51,9 @@ public class JerryRat implements Runnable {
                     String s2;
                     String s = URLDecoder.decode(st, "utf-8");
                     String s1;
-                    if (ifHead) {
+                    if(ifHead){
                         s1 = s.substring(5);
-                    } else {
+                    }else {
                         s1 = s.substring(4);
                     }
 
@@ -63,22 +64,22 @@ public class JerryRat implements Runnable {
                         s2 = s1;
                     }
                     if (s2.equals("/endpoints/user-agent")) {
-                        while ((OOOO = inputStream.read()) != -1) {
-                            if (OOOO == 13) {
-                                OOOO = inputStream.read();
-                                if (OOOO == 10) {
-                                    OOOO = inputStream.read();
-                                    a.add((byte) 13);
-                                    a.add((byte) 10);
-                                    if (OOOO == 13) {
-                                        OOOO = inputStream.read();
+                        while ((OOOO=inputStream.read()) != -1){
+                            if (OOOO==13) {
+                                OOOO=inputStream.read();
+                                if (OOOO==10) {
+                                    OOOO=inputStream.read();
+                                    a.add((byte)13);
+                                    a.add((byte)10);
+                                    if (OOOO==13) {
+                                        OOOO=inputStream.read();
                                         break;
-                                    } else {
-                                        a.add((byte) OOOO);
+                                    }else{
+                                        a.add((byte)OOOO);
                                     }
                                 }
-                            } else {
-                                a.add((byte) OOOO);
+                            }else{
+                                a.add((byte)OOOO);
                             }
                         }
                         bytes = new byte[a.size()];
@@ -86,19 +87,22 @@ public class JerryRat implements Runnable {
                             bytes[i] = a.get(i);
                         }
                         bytes2String = new String(bytes, "UTF-8");
-                        split = bytes2String.split("\r\n", 2);
+                        split = bytes2String.split("\r\n",2);
                         st = split[0];
-                        split = split[1].split("\r\n", 2);
+                        split = split[1].split("\r\n",2);
                         String userAgent = split[0];
                         while (!userAgent.startsWith("User-Agent")) {
-                            split = split[1].split("\r\n", 2);
+                            split = split[1].split("\r\n",2);
                             userAgent = split[0];
                         }
                         String substring = userAgent.substring(12);
                         SimpleDateFormat sdf = new SimpleDateFormat("EEE, dd MMM yyyy hh:mm:ss z", Locale.ENGLISH);
                         sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
                         String str = sdf.format(new Date());
-                        clientSocket.getOutputStream().write(("HTTP/1.0 200 OK" + "\r\n" + "Date: " + str + "\r\n" + "Server: Apache/11.0" + "\r\n" + "Content-Length: " + substring.length() + "\r\n" + "Content-Type: " + "" + "text/html" + ";charset=utf-8" + "\r\n" + "\r\n").getBytes());
+                        clientSocket.getOutputStream().write(("HTTP/1.0 200 OK" + "\r\n" +
+                                "Date: " + str + "\r\n" + "Server: Apache/11.0" + "\r\n" +
+                                "Content-Length: " + substring.length() + "\r\n" + "Content-Type: " +
+                                "" + "text/html" + ";charset=utf-8" + "\r\n" + "\r\n").getBytes());
                         clientSocket.getOutputStream().write(substring.getBytes());
                     } else {
                         String pathname = "res/webroot" + s2;
@@ -194,92 +198,122 @@ public class JerryRat implements Runnable {
                             e.printStackTrace();
                         }
                     }
-                } else if (st.startsWith("POST") | st.startsWith("post")) {
-                    while ((OOOO = inputStream.read()) != -1) {
-                        if (OOOO == 13) {
-                            OOOO = inputStream.read();
-                            if (OOOO == 10) {
-                                OOOO = inputStream.read();
-                                a.add((byte) 13);
-                                a.add((byte) 10);
-                                if (OOOO == 13) {
-                                    OOOO = inputStream.read();
+                }
+                else if (st.startsWith("POST")|st.startsWith("post")){
+                while ((OOOO=inputStream.read()) != -1){
+                    if (OOOO==13) {
+                        OOOO=inputStream.read();
+                        if (OOOO==10) {
+                            OOOO=inputStream.read();
+                            a.add((byte)13);
+                            a.add((byte)10);
+                            if (OOOO==13) {
+                                OOOO=inputStream.read();
                                     break;
-                                } else {
-                                    a.add((byte) OOOO);
-                                }
+                            }else{
+                                a.add((byte)OOOO);
                             }
-                        } else {
-                            a.add((byte) OOOO);
+                        }
+                    }else{
+                        a.add((byte)OOOO);
+                    }
+                }
+                bytes = new byte[a.size()];
+                for (int i = 0; i < a.size(); i++) {
+                    bytes[i] = a.get(i);
+                }
+                bytes2String = new String(bytes, "UTF-8");
+                split = bytes2String.split("\r\n",2);
+                st = split[0];
+                String afterDecode = URLDecoder.decode(st, "utf-8");
+                String deletePost = afterDecode.substring(5);
+                String url = deletePost.substring(0, deletePost.length() - 9);
+                if (url.equals("/endpoints/null")) {
+                            File file = new File("res/webroot/null");
+                            if(!file.exists()){
+                                file.createNewFile();
+                            }
+                            split = split[1].split("\r\n",2);
+                            String contentLength = split[0];
+                            while (!contentLength.startsWith("Content-Length")) {
+                                split = split[1].split("\r\n",2);
+                                contentLength = split[0];
+                            }
+                            String[] splitLength = contentLength.split(": ");
+                            String length = splitLength[1];
+                            int lengthNumber = Integer.parseInt(length);
+                            split = split[1].split("\r\n",2);
+                            String black = split[0];
+                            while (!black.equals("")) {
+                                split = split[1].split("\r\n",2);
+                                black = split[0];
+                            }
+                            FileOutputStream fileOutputStream = new FileOutputStream("res/webroot/null");
+                            for (int i = 0; i <lengthNumber ; i++) {
+                                inputStream.read();
+                            }
+                            fileOutputStream.close();
+                            clientSocket.getOutputStream().write(("HTTP/1.0 204 Not Content" + "\r\n" + "\r\n").getBytes());
+                        }else {
+                            File file = new File("res/webroot/emails");
+                            if(!file.exists()){
+                                file.mkdirs();
+                            }
+                            String realUrl = "res/webroot"+url;
+                            File emailFail = new File(realUrl);
+                            try {
+                                emailFail.createNewFile();
+                            }catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                            split = split[1].split("\r\n",2);
+                            String contentLength = split[0];
+
+                            while (!contentLength.startsWith("Content-Length")&!contentLength.startsWith("Content-Type")) {
+                                split = split[1].split("\r\n",2);
+                                contentLength = split[0];
+
+                            }
+                            int lengthNumber = 0;
+                            String type = "";
+                            if (contentLength.startsWith("Content-Length")) {
+                                String[] splitLength = contentLength.split(": ");
+                                String length = splitLength[1];
+                                lengthNumber = Integer.parseInt(length);
+                                while (!contentLength.startsWith("Content-Type")) {
+                                    split = split[1].split("\r\n",2);
+                                    contentLength = split[0];
+                                }
+                                String[] split1 = contentLength.split(": ");
+                                type = split1[1];
+                            }else if (contentLength.startsWith("Content-Type")) {
+                                String[] split1 = contentLength.split(": ");
+                                type = split1[1];
+                                while (!contentLength.startsWith("Content-Length")) {
+                                    split = split[1].split("\r\n",2);
+                                    contentLength = split[0];
+                                }
+                                String[] splitL = contentLength.split(": ");
+                                String length = splitL[1];
+                                lengthNumber = Integer.parseInt(length);
+                            }
+
+                            FileOutputStream fileOutputStream = new FileOutputStream(emailFail,false);
+                            for (int i = 0; i < lengthNumber ; i++) {
+                                fileOutputStream.write(inputStream.read());
+                            }
+                            fileOutputStream.close();
+                            clientSocket.getOutputStream().write(("HTTP/1.0 200 OK" + "\r\n" + "\r\n").getBytes());
                         }
                     }
-                    bytes = new byte[a.size()];
-                    for (int i = 0; i < a.size(); i++) {
-                        bytes[i] = a.get(i);
-                    }
-                    bytes2String = new String(bytes, "UTF-8");
-                    split = bytes2String.split("\r\n", 2);
-                    st = split[0];
-                    String afterDecode = URLDecoder.decode(st, "utf-8");
-                    String deletePost = afterDecode.substring(5);
-                    String url = deletePost.substring(0, deletePost.length() - 9);
-                    if (url.equals("/endpoints/null")) {
-                        File file = new File("res/webroot/null");
-                        if (!file.exists()) {
-                            file.createNewFile();
-                        }
-                        split = split[1].split("\r\n", 2);
-                        String contentLength = split[0];
-                        while (!contentLength.startsWith("Content-Length")) {
-                            split = split[1].split("\r\n", 2);
-                            contentLength = split[0];
-                        }
-                        int lengthNumber = 0;
-                        String[] splitLength = contentLength.split(": ");
-                        String length = splitLength[1];
-                        lengthNumber = Integer.parseInt(length);
-                        FileOutputStream fileOutputStream = new FileOutputStream("res/webroot/null");
-                        for (int i = 0; i < lengthNumber; i++) {
-                            inputStream.read();
-                        }
-                        fileOutputStream.close();
-                        clientSocket.getOutputStream().write(("HTTP/1.0 204 Not Content" + "\r\n" + "\r\n").getBytes());
-                    } else {
-                        File file = new File("res/webroot/emails");
-                        if (!file.exists()) {
-                            file.mkdirs();
-                        }
-                        String realUrl = "res/webroot" + url;
-                        File emailFail = new File(realUrl);
-                        try {
-                            emailFail.createNewFile();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                        split = split[1].split("\r\n", 2);
-                        String contentLength = split[0];
-                        while (!contentLength.startsWith("Content-Length")) {
-                            split = split[1].split("\r\n", 2);
-                            contentLength = split[0];
-                        }
-                        int lengthNumber = 0;
-                        String[] splitLength = contentLength.split(": ");
-                        String length = splitLength[1];
-                        lengthNumber = Integer.parseInt(length);
-                        FileOutputStream fileOutputStream = new FileOutputStream(emailFail, false);
-                        for (int i = 0; i < lengthNumber; i++) {
-                            fileOutputStream.write(inputStream.read());
-                        }
-                        fileOutputStream.close();
-                        clientSocket.getOutputStream().write(("HTTP/1.0 200 OK" + "\r\n" + "\r\n").getBytes());
-                    }
+                else {
+                    clientSocket.getOutputStream().write(("HTTP/1.0 400 Bad Request" + "\r\n" + "\r\n").getBytes());
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
-
     public static void main(String[] args) throws IOException {
         JerryRat jerryRat = new JerryRat();
         new Thread(jerryRat).run();
